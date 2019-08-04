2e58ddb98ef1741243537ac1f9bfab18772e0d08
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-08-04 11:05:46 -0600

    Agregar ViewModel para la vista Facturas

---
+++ FacturasOsprint/ViewModel/FacturasViewModel.cs
@@ -0,0 +1,33 @@
+﻿using FacturasOsprint.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacturasOsprint.ViewModel
+{
+    internal class FacturasViewModel : ViewModelBase
+    {
+        public Factura Factura { get; set; }
+        
+        public FacturasViewModel()
+        {
+            Factura = new Factura();
+            Factura.Fecha = DateTime.Now;
+            WireCommands();
+        }
+
+        public void WireCommands()
+        {
+            CalcularTotalCommand = new RelayCommand(CalcularTotal);
+        }
+
+        public RelayCommand CalcularTotalCommand { get; set; }
+
+        private void CalcularTotal()
+        {
+            Factura.Importe15 = Factura.Servicios.Sum(x => x.Total);
+        }
+    }
+}
