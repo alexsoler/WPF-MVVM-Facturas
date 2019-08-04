e6e36375c8eaa3d272087e873fb1dc2abd1c23d5
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-08-04 11:01:44 -0600

    Agregar clase modelo Servicio

---
+++ FacturasOsprint/Model/Servicio.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacturasOsprint.Model
+{
+    public class Servicio : ModelBase
+    {
+        private int cantidad;
+
+        public int Cantidad
+        {
+            get { return cantidad; }
+            set
+            {
+                if (cantidad == value) return;
+                cantidad = value;
+                OnPropertyChanged();
+                CalcularTotal();
+            }
+        }
+
+        private string descripcion;
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set
+            {
+                if (descripcion == value) return;
+                descripcion = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private decimal precioUnitario;
+
+        public decimal PrecioUnitario
+        {
+            get { return precioUnitario; }
+            set
+            {
+                if (precioUnitario == value) return;
+                precioUnitario = value;
+                OnPropertyChanged();
+                CalcularTotal();
+            }
+        }
+
+        private decimal descuento;
+
+        public decimal Descuento
+        {
+            get { return descuento; }
+            set
+            {
+                if (descuento == value) return;
+                descuento = value;
+                OnPropertyChanged();
+                CalcularTotal();
+            }
+        }
+
+        private decimal total;
+
+        public decimal Total
+        {
+            get { return total; }
+            set
+            {
+                if (total == value) return;
+                total = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void CalcularTotal()
+        {
+            Total = Cantidad * PrecioUnitario - Descuento;
+        }
+    }
+}
