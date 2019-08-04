0a0f408b685a2ab73bb1c0d4c88fa55734679a69
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-08-04 11:04:38 -0600

    Agregar implementaciones para ICommand

---
+++ FacturasOsprint/ViewModel/ParamCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FacturasOsprint.ViewModel
+{
+    internal class ParamCommand : ICommand
+    {
+        private Action<object> _action;
+        private readonly Func<bool> _canExecute;
+
+        public ParamCommand(Action<object> action)
+        {
+            _action = action;
+            _canExecute = () => true;
+        }
+
+        public ParamCommand(Action<object> action, Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (_canExecute == null)
+            {
+                return true;
+            }
+            else
+            {
+                bool result = _canExecute.Invoke();
+                return result;
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(parameter))
+            {
+                if (parameter != null)
+                {
+                    _action(parameter);
+                }
+            }
+        }
+    }
+}
---
+++ FacturasOsprint/ViewModel/RelayCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FacturasOsprint.ViewModel
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action _action;
+        private readonly Func<bool> _canExecute;
+
+        public RelayCommand(Action action, Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public RelayCommand(Action action)
+        {
+            _action = action;
+            _canExecute = () => true;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            bool result = this._canExecute.Invoke();
+            return result;
+        }
+
+        public void Execute(object parameter)
+        {
+            this._action.Invoke();
+        }
+    }
+}
