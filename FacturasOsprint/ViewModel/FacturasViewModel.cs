2b254f035845cf565a2d85d4963de7de46ddf5ea
alexsoler <40123834+alexsoler@users.noreply.github.com>
2019-08-04 19:47:26 -0600

    Refactorizar ViewModel Facturas

--- FacturasOsprint/ViewModel/FacturasViewModel.cs
+++ FacturasOsprint/ViewModel/FacturasViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using FacturasOsprint.Model;
+using FacturasOsprint.ReportsView;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,25 +10,63 @@ namespace FacturasOsprint.ViewModel
 {
     internal class FacturasViewModel : ViewModelBase
     {
-        public Factura Factura { get; set; }
-        
+        #region Propiedades
+        private Factura _factura;
+        public Factura Factura
+        {
+            get => _factura;
+            set
+            {
+                if (_factura == value) return;
+                _factura = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
         public FacturasViewModel()
         {
             Factura = new Factura();
-            Factura.Fecha = DateTime.Now;
             WireCommands();
         }
 
+        #region Comandos
         public void WireCommands()
         {
             CalcularTotalCommand = new RelayCommand(CalcularTotal);
+            GenerarFacturaCommand = new RelayCommand(GenerarFactura);
+            NuevaFacturaCommand = new RelayCommand(NuevaFactura);
         }
+        
 
         public RelayCommand CalcularTotalCommand { get; set; }
+        public RelayCommand GenerarFacturaCommand { get; set; }
+        public RelayCommand NuevaFacturaCommand { get; set; }
+        #endregion
 
+        #region Metodos
         private void CalcularTotal()
         {
             Factura.Importe15 = Factura.Servicios.Sum(x => x.Total);
         }
+
+        public void GenerarFactura()
+        {
+            int i = 1;
+            foreach (var item in Factura.Servicios)
+            {
+                item.Id = i;
+                i++;
+            }
+
+            FormFactura frm = new FormFactura(Factura);
+            frm.Show();
+        }
+
+        public void NuevaFactura()
+        {
+            Factura = new Factura();
+        }
+        #endregion
     }
 }
